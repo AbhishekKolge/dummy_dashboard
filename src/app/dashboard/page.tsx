7e25c23fe8a1289baea30a69a297d5e0ae749441
AbhishekUnthread <abhishek.k@unthread.world>
2024-02-18 11:53:26 +0530

    suspense and loader added for rental tools

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,15 +1,20 @@
+import { Suspense } from 'react';
+
 import RentalTools from '@/components/dashboard/rental-tools';
 import WorkOrderStatus from '@/components/dashboard/work-order-status';
 import MissingItems from '@/components/dashboard/missing-items';
 import ToolsEquipmentsAvailability from '@/components/dashboard/tools-equipments-availability';
 import RestockItems from '@/components/dashboard/restock-items';
 import ReceptionSummary from '@/components/dashboard/reception-summary';
+import RentalToolsLoading from '@/components/dashboard/rental-tools/loading';
 
 const Dashboard: React.FC = () => {
   return (
     <section className='grid grid-cols-5 gap-4'>
       <div className='w-full col-span-3'>
-        <RentalTools />
+        <Suspense fallback={<RentalToolsLoading />}>
+          <RentalTools />
+        </Suspense>
       </div>
       <div className='w-full col-span-2'>
         <WorkOrderStatus />
