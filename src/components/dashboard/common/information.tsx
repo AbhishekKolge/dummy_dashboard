3fb1b6819024e75e6db00cb12ebd96af443f0559
AbhishekUnthread <abhishek.k@unthread.world>
2024-02-19 00:18:28 +0530

    ui changes

--- src/components/dashboard/common/information.tsx
+++ src/components/dashboard/common/information.tsx
@@ -6,20 +6,22 @@ const Information: React.FC<InformationProps> = (props) => {
   const { data, enableChart } = props;
 
   return (
-    <div className='grid gap-1'>
+    <div className='grid gap-1 group-[[data-title-bold=true]]:gap-6'>
       {data.map((item, index) => {
         return (
           <div key={index} className='flex items-center justify-between'>
             <div>
-              <item.icon size={42} strokeWidth={1.2} />
+              <item.icon size={46} strokeWidth={1.2} />
             </div>
             <div className='font-light group-[[data-title-bold=true]]:font-bold text-lg'>
               {item.name}
             </div>
-            {enableChart && (
+            {enableChart && item?.total ? (
               <div className='w-[90px] h-[90px]'>
                 <DonutChart count={item.count} total={item.total} />
               </div>
+            ) : (
+              <div className='font-bold text-lg'>{item.count}</div>
             )}
           </div>
         );
