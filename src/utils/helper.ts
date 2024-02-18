4dd6f6490d66cce0a269cb3f3e941c86427693a2
AbhishekUnthread <abhishek.k@unthread.world>
2024-02-18 11:50:06 +0530

    wait utility function added to mimic api response time

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,7 +1,13 @@
+import { WAIT_DURATION } from './defaults';
+
 export const getInitials = (str: string): string => {
   const words = str.split(' ');
   const firstTwoLetters = words.map((word) => word.charAt(0)).slice(0, 2);
   const result = firstTwoLetters.join('');
 
   return result;
+};
+
+export const wait = (duration = WAIT_DURATION) => {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 };
