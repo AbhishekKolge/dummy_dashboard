28b2288f92470ed3e153e8612f97df87ec8a53bd
AbhishekUnthread <abhishek.k@unthread.world>
2024-02-18 14:18:48 +0530

    get rental tools service created

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,6 @@ import { wait } from '@/utils/helper';
 import { RENTAL_TOOLS_DATA } from '@/components/dashboard/data';
 
 export const getRentalTools = async () => {
-  await wait();
+  await wait(0);
   return RENTAL_TOOLS_DATA;
 };
