07b6f58ce48062f4275b5e4d17054dccbb8f758d
AbhishekUnthread <abhishek.k@unthread.world>
2024-02-18 20:18:44 +0530

    restock items async component created

--- src/components/dashboard/restock-items/index.tsx
+++ src/components/dashboard/restock-items/index.tsx
@@ -1,12 +1,18 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import RestockItemsTable from './table';
 
-const RestockItems: React.FC = () => {
+import { getRestockItems } from '@/services';
+
+const RestockItems: React.FC = async () => {
+  const data = await getRestockItems();
   return (
     <Card>
       <CardHeader>
         <CardTitle>Restock Items</CardTitle>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent className='px-10 pb-8 pt-0'>
+        <RestockItemsTable data={data} />
+      </CardContent>
     </Card>
   );
 };
