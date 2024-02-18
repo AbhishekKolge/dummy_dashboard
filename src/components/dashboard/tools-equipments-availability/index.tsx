20715713a3278713e2149dfa906da693524a1d73
AbhishekUnthread <abhishek.k@unthread.world>
2024-02-18 22:37:48 +0530

    tools availability feature added

--- src/components/dashboard/tools-equipments-availability/index.tsx
+++ src/components/dashboard/tools-equipments-availability/index.tsx
@@ -1,12 +1,19 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
-const ToolsEquipmentsAvailability: React.FC = () => {
+import { getToolsEquipmentsAvailability } from '@/services';
+import Information from '../common/information';
+
+const ToolsEquipmentsAvailability: React.FC = async (props) => {
+  const data = await getToolsEquipmentsAvailability();
+
   return (
     <Card>
       <CardHeader>
         <CardTitle>Tools and Equipments Availability</CardTitle>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent className='px-10 pb-8 pt-0 group'>
+        <Information data={data} enableChart={true} />
+      </CardContent>
     </Card>
   );
 };
