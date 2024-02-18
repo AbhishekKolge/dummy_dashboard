bd8abaad1e66042a52ff0b6b01a19db087663ac6
AbhishekUnthread <abhishek.k@unthread.world>
2024-02-18 19:27:35 +0530

    new defaults added

--- src/utils/defaults.ts
+++ src/utils/defaults.ts
@@ -2,3 +2,28 @@ export const TOOLTIP_DELAY_DURATION = 0;
 export const THEME = 'system';
 export const THEME_ATTRIBUTE = 'class';
 export const WAIT_DURATION = 3000;
+export const LEVEL = {
+  1: '🥉',
+  2: '🏆',
+  3: '⭐️',
+};
+export const ONE_MINUTE = 60000;
+export const ONE_SECOND = 1000;
+export const STATUS = {
+  completed: {
+    color: '#22c55e',
+    text: 'completed',
+  },
+  inProgress: {
+    color: '#2563eb',
+    text: 'in progress',
+  },
+  notStarted: {
+    color: '#dc2626',
+    text: 'not started',
+  },
+  onHold: {
+    color: '#d97706',
+    text: 'on hold',
+  },
+};
