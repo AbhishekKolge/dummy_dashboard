a75d0f14d1a21892981bdfb7b66d832fc151501f
AbhishekUnthread <abhishek.k@unthread.world>
2024-02-18 14:17:43 +0530

    dummy response data added for rental tools

--- src/components/dashboard/data.ts
+++ src/components/dashboard/data.ts
@@ -13,4 +13,28 @@ export const RENTAL_TOOLS_DATA: RentalTool[] = [
     workStatus: 'completed',
     duration: 900000,
   },
+  {
+    order: 2,
+    toolRef: 5665,
+    member: {
+      firstName: 'Razib',
+      lastName: 'Rahman',
+      profileImage: 'https://randomuser.me/api/portraits/men/2.jpg',
+      level: 2,
+    },
+    workStatus: 'inProgress',
+    duration: 300000,
+  },
+  {
+    order: 3,
+    toolRef: 1755,
+    member: {
+      firstName: 'Luke',
+      lastName: 'Norton',
+      profileImage: 'https://randomuser.me/api/portraits/men/3.jpg',
+      level: 3,
+    },
+    workStatus: 'notStarted',
+    duration: 0,
+  },
 ];
